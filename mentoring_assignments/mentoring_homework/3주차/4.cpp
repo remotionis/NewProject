7f859a26cce1af822de852a914521c91f1f69625
remotionis <remotion371@soongsil.ac.kr>
2021-02-18 23:48:55 +0900

    Create 4.cpp

---
+++ mentoring_assignments/mentoring_homework/3주차/4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class Calculator {
+	private:
+		float num1, num2, result;
+		char ch;
+	public:
+}
+
+int main()
+{
+	Calculator Cal;
+	std::cin >> num1 >> ch >> num2;
+	std::cout << num1 << " " << ch << " " << num2 << " = " << result;
+	return 0;
+}
